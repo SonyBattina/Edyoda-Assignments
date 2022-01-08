14b0fbb82e5889db709a423653c92f97930c517b
SonyBattina <90275218+SonyBattina@users.noreply.github.com>
2022-01-08 21:40:02 +0530

    Add files via upload

---
+++ Pyquiz.py
@@ -0,0 +1,11 @@
+quiz = { 1 : {"question" : "Which of the following is the correct extension of the Python file? \n a .xml \n b .csv \n c .python \n d .py",
+        "answer" : "d"},
+         2 : {"question" : "Which keyword is used for function in Python language? \n a.function \n b.fun \n c.def \n d.define" ,
+        "answer" : "c"},
+             3 : {"question" : "To add a new element to a list we use which Python command? \n a.list.append() \n b.list.add() \n c.list.sum()          \n d.list.new()",
+        "answer" : "a"},
+    4 : {"question" : "What are the two main types of functions in Python? \n a.built in \n b.userdefined \n c.both \n d.none",
+        "answer" : "c"},
+    5 : {"question" : "What is output of print(math.pow(3, 2))? \n a.9 \n b.9.0 \n c.9.00 \n d.6.0",
+        "answer" : "b"}
+         }
